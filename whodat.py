2460b2ee79c616ace16a45d7596e99a73737de47
Pr0t3an <admin@pr0t3an.com>
2017-10-04 22:50:00 +0100

    Create whodat.py

---
+++ whodat.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+# uses pycountry
+# pip install pycountry
+# Super basic script Alpha_2 to Country Name
+# Embarrassed to event publish - but will save me a bit of time finding it in the future
+
+import sys
+import pycountry
+if len(sys.argv) == 2:
+    try:
+        country = pycountry.countries.get(alpha_2=sys.argv[1].upper())
+        print '\n' + country.name + '\n'
+    except:
+        "Not Found...sorry"
+else:
+    print "Need to enter in a 2 Letter country code.. whodat.py DE"
+
+
+
